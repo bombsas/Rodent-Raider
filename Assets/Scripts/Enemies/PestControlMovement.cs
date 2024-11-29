3707fc1df681762b42199aceb0002ad068f5578e
Huu <huuhuynh1999@gmail.com>
2024-11-29 05:45:39 -0700

    movement works on enemies however sprite messed up

--- Assets/Scripts/Enemies/PestControlMovement.cs
+++ Assets/Scripts/Enemies/PestControlMovement.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class PestControlMovement : MonoBehaviour
 {
-    [SerializeField] private float speed;
-    [SerializeField] private float jumpPower;
-    [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private LayerMask wallLayer;
-    private Rigidbody2D body;
-    private Animator anim;
-    private BoxCollider2D boxCollider;
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float movementDistance;
+    [SerializeField] private float movementSpeed;
+
+    private bool movingLeft;
+    private float leftEdge;
+    private float rightEdge;
+
+    private void Awake()
     {
-        
+        leftEdge = transform.position.x - movementDistance;
+        rightEdge = transform.position.y + movementDistance;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (movingLeft)
+        {
+            if (transform.position.x > leftEdge)
+            {
+                transform.position = new Vector3(transform.position.x - movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            else
+            {
+                movingLeft = false;
+            }
+        }
+        else
+        {
+            if (transform.position.x < rightEdge)
+            {
+                transform.position = new Vector3(transform.position.x + movementSpeed * Time.deltaTime, transform.position.y, transform.position.z);
+                transform.localScale = new Vector3(-1, 1, 1);
+            }
+            else
+            {
+                movingLeft = true;
+            }
+        }
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            bool stunned = collision.GetComponent<Health>().gothitcheck;
+            if (stunned == false)
+                collision.GetComponent<Health>().beenDamaged();
+        }
     }
 }
